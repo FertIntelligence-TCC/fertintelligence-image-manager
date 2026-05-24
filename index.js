1efc367583c115724acdb01ae93f99c2b42438c7
Miguel Macedo Ferreira <132075645+Mikeru-UFCG@users.noreply.github.com>
2026-05-24 07:07:35 -0300

    Refatora validações de imagem mantendo API atual

--- index.js
+++ index.js
@@ -8,86 +8,91 @@ const PORT = process.env.PORT || 8081
 
 const app = express()
 app.use(cors())
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-const bd_url = "mongodb+srv://mikerufcg:adubosdaora123@fertintelligence-tcc.smehfzj.mongodb.net/FertIntelligence?retryWrites=true&w=majority&appName=FertIntelligence-TCC";
+const bd_url = 'mongodb+srv://mikerufcg:adubosdaora123@fertintelligence-tcc.smehfzj.mongodb.net/FertIntelligence?retryWrites=true&w=majority&appName=FertIntelligence-TCC'
 
 mongoose.connect(bd_url)
-.then(() =>{
-    console.log("Connect to DB")
-}).catch((err) => {
+  .then(() => {
+    console.log('Connect to DB')
+  }).catch((err) => {
     console.log(err)
-})
+  })
 
 const schema = new mongoose.Schema({
-    image: String
+  image: String
 })
-const imageModel = mongoose.model("Image",schema)
+const imageModel = mongoose.model('Image', schema)
 
-app.get("/", async (req,res) => {
-    //const data = await imageModel.find({})
-    res.json({message : "Server running", data: res.statusCode})
-})
+const isValidImagePayload = (value) => typeof value === 'string' && value.trim().length > 0
+const getImageFromPayload = (body) => {
+  if (isValidImagePayload(body?.image)) return body.image
+  if (isValidImagePayload(body?.img)) return body.img
+  return null
+}
 
-app.get("/get/:id", async (req,res) => {
-    if (ObjectId.isValid(req.params.id)) {
-        const id = req.body
-        const data = await imageModel.findOne({_id: new ObjectId(req.params.id)}).then(doc => {
-            res.status(200).json(doc)
-        })
-        .catch(err => {
-            res.status(500).json({error:'Could not fetch the document'})
-        })
-    } else {
-        res.status(500).json({error: 'Could not fetch the document'})
-    }
+app.get('/', async (req, res) => {
+  res.json({ message: 'Server running', data: res.statusCode })
 })
 
-app.post("/upload", async (req,res) => {
-    const image = new imageModel({
-        image: req.body.img
+app.get('/get/:id', async (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    imageModel.findOne({ _id: new ObjectId(req.params.id) }).then((doc) => {
+      res.status(200).json(doc)
     })
-    const data = await image.save()
-    res.send({data})
+      .catch(() => {
+        res.status(500).json({ error: 'Could not fetch the document' })
+      })
+  } else {
+    res.status(500).json({ error: 'Could not fetch the document' })
+  }
+})
+
+app.post('/upload', async (req, res) => {
+  const image = getImageFromPayload({ img: req.body?.img })
+
+  if (!image) {
+    return res.status(400).json({ error: 'Missing required field: img' })
+  }
+
+  const data = await new imageModel({ image }).save()
+  return res.send({ data })
 })
 
 app.delete('/delete/:id', (req, res) => {
-
-    if (ObjectId.isValid(req.params.id)) {
-  
+  if (ObjectId.isValid(req.params.id)) {
     imageModel
       .deleteOne({ _id: new ObjectId(req.params.id) })
-      .then(result => {
+      .then((result) => {
         res.status(200).json(result)
       })
-      .catch(err => {
-        res.status(500).json({error: 'Could not delete document'})
+      .catch(() => {
+        res.status(500).json({ error: 'Could not delete document' })
       })
-  
-    } else {
-      res.status(500).json({error: 'Could not delete document'})
-    }
+  } else {
+    res.status(500).json({ error: 'Could not delete document' })
+  }
 })
 
 app.patch('/update/:id', (req, res) => {
-    const updates = req.body
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ error: 'Could not update document' })
+  }
 
-    console.log(req.body)
-    if (ObjectId.isValid(req.params.id)) {
-        imageModel
-        .updateOne({ _id: new ObjectId(req.params.id) }, {$set: updates})
-        .then(result => {
-          console.log(req.params)
-          res.status(200).json(result)
-        })
-        .catch(err => {
-          res.status(500).json({error: 'Could not update document'})
-        })
-  
-    } else {
-      res.status(500).json({error: 'Could not update document'})
-    }
+  const image = getImageFromPayload(req.body)
+  if (!image) {
+    return res.status(400).json({ error: 'Missing required field: image or img' })
+  }
+
+  imageModel
+    .updateOne({ _id: new ObjectId(req.params.id) }, { $set: { image } })
+    .then((result) => {
+      res.status(200).json(result)
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'Could not update document' })
+    })
 })
 
-app.listen(PORT, ()=>console.log("Server is running at "+PORT))
+app.listen(PORT, () => console.log('Server is running at ' + PORT))
